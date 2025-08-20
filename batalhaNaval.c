77768a3b18bf700fc0988007d6e1ed0af9bd1046
Yan Carvalho <yan.carvalho.profissional@gmail.com>
2025-08-20 01:36:03 +0000

    DESAFIONAVAL

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,24 +1,34 @@
 #include <stdio.h>
 
 int main() {
-    // Tabuleiro com 11 linhas (0 a 10) e 10 colunas (A a J)
-    char tabuleiro[11][10];
+    // Tabuleiro 10x10: linhas 0 a 9, colunas A a J
+    int tabuleiro[10][10];
 
-    // Inicializa com água (~)
-    for (int i = 0; i < 11; i++) {
+    // Inicializa com água (0)
+    for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
-            tabuleiro[i][j] = '~';
+            tabuleiro[i][j] = 0;
         }
     }
 
     // Navio vertical: começa em linha 2, coluna C (índice 2, 2), tamanho 3
     for (int i = 0; i < 3; i++) {
-        tabuleiro[2 + i][2] = 'N';
+        tabuleiro[2 + i][2] = 3;
     }
 
     // Navio horizontal: começa em linha 6, coluna B (índice 6, 1), tamanho 3
     for (int i = 0; i < 3; i++) {
-        tabuleiro[6][1 + i] = 'N';
+        tabuleiro[6][1 + i] = 3;
+    }
+
+    // Navio diagonal 1: começa em linha 0, coluna 0, tamanho 3 (↘)
+    for (int i = 0; i < 3; i++) {
+        tabuleiro[0 + i][0 + i] = 3;
+    }
+
+    // Navio diagonal 2: começa em linha 3, coluna 6, tamanho 3 (↙)
+    for (int i = 0; i < 3; i++) {
+        tabuleiro[3 + i][6 - i] = 3;
     }
 
     // Imprime cabeçalho das colunas com letras
@@ -28,11 +38,11 @@ int main() {
     }
     printf("\n");
 
-    // Imprime linhas com números de 0 a 10
-    for (int i = 0; i < 11; i++) {
-        printf("%2d ", i); // número da linha
+    // Imprime linhas com números de 0 a 9
+    for (int i = 0; i < 10; i++) {
+        printf("%2d ", i);
         for (int j = 0; j < 10; j++) {
-            printf("%c ", tabuleiro[i][j]);
+            printf("%d ", tabuleiro[i][j]);
         }
         printf("\n");
     }
